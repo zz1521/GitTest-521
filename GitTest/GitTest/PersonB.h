8753febeff65455441d523cb811a115c48cca2dd
zz1521 <719507339@qq.com>
2016-04-01 15:04:10 +0800

    PersonB类创建

---
+++ GitTest/GitTest/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  GitTest
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 LiPengfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
---
+++ GitTest/GitTest/PersonB.m
@@ -0,0 +1,13 @@
+//
+//  PersonB.m
+//  GitTest
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 LiPengfei. All rights reserved.
+//
+
+#import "PersonB.h"
+
+@implementation PersonB
+
+@end
